796235db6e7ee2d55fa1abe0338be1aca2bb95ae
Erickson Silva <erickson.silva@lavid.ufpb.br>
2015-03-10 08:35:55 -0300

    Adiciona include da quantidade de dados no BD na Home

--- src/main/java/br/com/petshopplus/controller/IndexController.java
+++ src/main/java/br/com/petshopplus/controller/IndexController.java
@@ -5,7 +5,12 @@ import javax.inject.Inject;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
+import br.com.petshopplus.dao.AgendaDao;
+import br.com.petshopplus.dao.AnimalDao;
+import br.com.petshopplus.dao.ClienteDao;
 import br.com.petshopplus.dao.LoginDao;
+import br.com.petshopplus.dao.ProdutoDao;
+import br.com.petshopplus.dao.ServicoDao;
 import br.com.petshopplus.model.Login;
 import br.com.petshopplus.safety.Restrito;
 import br.com.petshopplus.safety.Session;
@@ -15,20 +20,35 @@ public class IndexController {
 
 	private final Result result;
 	private final LoginDao loginDao;
+	private final AnimalDao animalDao;
+	private final ClienteDao clienteDao;
+	private final ServicoDao servicoDao;
+	private final ProdutoDao produtoDao;
+	private final AgendaDao agendaDao;
 	
 	protected IndexController() {
-		this(null, null);
+		this(null, null, null,  null, null, null, null);
 	}
 	
 	@Inject
-	public IndexController(Result result, LoginDao loginDao){
+	public IndexController(Result result, LoginDao loginDao, AnimalDao animalDao, ClienteDao clienteDao, ServicoDao servicoDao, ProdutoDao produtoDao, AgendaDao agendaDao){
 		this.result = result;
 		this.loginDao = loginDao;
+		this.animalDao = animalDao;
+		this.clienteDao = clienteDao;
+		this.servicoDao = servicoDao;
+		this.produtoDao = produtoDao;
+		this.agendaDao = agendaDao;
 	}
 	
 	@Path("/home")
 	@Restrito
 	public void home() {
+		result.include("animais", animalDao.lista().size());
+		result.include("clientes", clienteDao.lista().size());
+		result.include("servicos", servicoDao.lista().size());
+		result.include("produtos", produtoDao.lista().size());
+		result.include("consultas", agendaDao.lista().size());
 	}
 
 	@Path("/login")
